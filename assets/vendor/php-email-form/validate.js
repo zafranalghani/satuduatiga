4c54393523f6ad0c8dda206cb831fe9dc7a47c98
zafranalghani <zafranalghanib@gmail.com>
2026-05-27 20:58:15 +0700

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -64,7 +64,7 @@
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
+      if (data.trim() == 'OK' || data.includes('"ok":true') || data.includes('success')) {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
